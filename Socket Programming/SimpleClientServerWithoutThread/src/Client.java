6a1ad079f27d5ba1077ca6eb458beff7cd18d9ef
Jakir Hasan <52996649+Jak57@users.noreply.github.com>
2024-05-05 09:41:01 +0600

    Update Client.java

--- Socket Programming/SimpleClientServerWithoutThread/src/Client.java
+++ Socket Programming/SimpleClientServerWithoutThread/src/Client.java
@@ -5,24 +5,28 @@ import java.net.Socket;
 import java.util.Scanner;
 
 public class Client {
-    public static void main(String args[]) throws IOException {
-        System.out.println("Client started...");
-        Socket socket = new Socket("127.0.0.1", 22222); // 127.0.0.1 -> IP address of localhost; 22222 -> Port number of server
-        System.out.println("Client connected...");
+    public static void main(String[] args) throws IOException {
+        System.out.println("CLIENT started...");
+        Socket clientSocket = new Socket("127.0.0.1", 22222);
+        System.out.println("SERVER connected...");
 
-        ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
-        ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
+        while (true) {
+            System.out.print("\nPlease enter message: ");
+            Scanner scanner = new Scanner(System.in);
+            String clientMsg = scanner.nextLine();
 
-        Scanner sc = new Scanner(System.in); // taking input from console
-        System.out.print("Enter: ");
-        String message = sc.nextLine(); // taking input as string
-        oos.writeObject(message); // sent to server
+            ObjectOutputStream oos = new ObjectOutputStream(clientSocket.getOutputStream());
+            ObjectInputStream ois = new ObjectInputStream(clientSocket.getInputStream());
 
-        try {
-            Object fromServer = ois.readObject(); // receiving object from server
-            System.out.println("From server: " + (String)fromServer);
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
+            oos.writeObject(clientMsg);
+            System.out.println("CLIENT sent: " + clientMsg);
+
+            try {
+                Object fromServer = ois.readObject();
+                System.out.println("CLIENT received: " + (String) fromServer);
+            } catch (ClassNotFoundException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 }
