7624ebd50abf09d10b9af0eb0d4d50cb9f86643c
Jakir Hasan <52996649+Jak57@users.noreply.github.com>
2024-05-05 09:40:11 +0600

    Update Server.java

--- Socket Programming/SimpleClientServerWithoutThread/src/Server.java
+++ Socket Programming/SimpleClientServerWithoutThread/src/Server.java
@@ -5,29 +5,25 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
-    public static void main(String args[]) throws IOException {
-        // Creating object of server socket.
-        // Exception can occur -> socket may be off / occupied
-        ServerSocket serverSocket = new ServerSocket(22222); // 22222 -> port number
-        System.out.println("Server started...");
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(22222);
+        System.out.println("SERVER started...");
+        Socket socket = serverSocket.accept();
+        System.out.println("CLIENT connected...");
 
-        while (true) { // server will do the below tasks continuously
-
-            Socket socket = serverSocket.accept(); // dedicated socket is provided to each client
-            System.out.println("Client connected...");
-
+        while (true) {
             ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
             ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
 
             try {
-                Object cMsg = ois.readObject(); // reading what client has sent
-                System.out.println("From client: " + (String) cMsg); // type casting Object to String
+                Object clientMsg = ois.readObject();
+                System.out.println("\nSERVER received: " + (String)clientMsg);
 
-                String serverMsg = (String) cMsg;
-                serverMsg = serverMsg.toUpperCase(); // uppercasing message in server side which is sent by client
+                String serverMsg = (String) clientMsg;
+                serverMsg = serverMsg.toUpperCase();
 
-                oos.writeObject(serverMsg); // sending message to client
-
+                oos.writeObject(serverMsg);
+                System.out.println("SERVER sent: " + serverMsg);
             } catch (ClassNotFoundException e) {
                 throw new RuntimeException(e);
             }
