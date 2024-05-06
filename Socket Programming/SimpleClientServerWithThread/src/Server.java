060b455fd83dd7a710a43c708bfd8ab3f7d14f45
Jakir Hasan <52996649+Jak57@users.noreply.github.com>
2024-05-06 07:33:48 +0600

    Update Server.java

--- Socket Programming/SimpleClientServerWithThread/src/Server.java
+++ Socket Programming/SimpleClientServerWithThread/src/Server.java
@@ -1,3 +1,5 @@
+// v1
+
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
@@ -56,6 +58,78 @@ class ServerThread implements Runnable {
             clientSocket.close();
         } catch (IOException e) {
             throw new RuntimeException(e);
+        }
+    }
+}
+
+
+// v2
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+class ServerThread implements Runnable {
+    Socket socket;
+    Thread t;
+    ServerThread(Socket socket) {
+        this.socket = socket;
+        t = new Thread(this);
+        t.start();
+    }
+
+    @Override
+    public void run() {
+        ObjectInputStream ois = null;
+        try {
+            ois = new ObjectInputStream(socket.getInputStream());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        ObjectOutputStream oos = null;
+        try {
+            oos = new ObjectOutputStream(socket.getOutputStream());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        while (true) {
+            try {
+                Object cMsg = ois.readObject();
+
+                if (cMsg == null)
+                    break;
+
+                String sMsg = (String) cMsg;
+
+                System.out.println("\nSERVER received: " + sMsg);
+                sMsg = sMsg.toUpperCase();
+
+                oos.writeObject(sMsg);
+                System.out.println("SERVER sent: " + sMsg);
+            } catch (ClassNotFoundException | IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        try {
+            socket.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
+
+public class Server {
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(22222);
+        System.out.println("Server started...");
+
+        while (true) {
+            Socket socket = serverSocket.accept();
+            new ServerThread(socket);
         }
     }
 }
